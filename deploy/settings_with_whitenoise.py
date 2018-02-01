2f557865c7550be740e5b8801001b5db3e79bd96
rivest829 <956605485@qq.com>
2018-02-01 13:56:51 +0800

    beta3.1.1【需求】备份正式环境中的settings.py

--- deploy/settings_with_whitenoise.py
+++ deploy/settings_with_whitenoise.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-#if use Gunicorn to run django ,please install "whitenoise" to handle the static file
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -34,15 +34,15 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'bootstrap3',
     'murong',
-    #'gunicorn',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     #'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +116,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = './static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 TEMPLATE_DIRS = ('./templates',)
