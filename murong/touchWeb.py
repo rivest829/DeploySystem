ed0d0fd619b2cf938995bd9dba679dd72f8eb401
rivest829 <956605485@qq.com>
2018-03-09 14:59:41 +0800

    beta4.7.4【BUG】刷新工程不再记录日志

--- murong/touchWeb.py
+++ murong/touchWeb.py
@@ -14,13 +14,9 @@ def touchWeb(request):
             servername, servername, target)
         do_dellog = os.popen(do_fab)
         log_outer = do_dellog.read().decode('gb18030').encode('utf-8')
-        log_path = os.path.join('exeLog', "touchLog-" + time.strftime("%Y%m%d-%H%M", time.localtime()) + ".txt")
-        with open(log_path, mode='a') as f:
-            f.write(log_outer + '**************operator:' + user)
         result = log_outer.split('[')
-        log_info = '本次执行日志已保存至' + log_path
         return render_to_response("resultDeploy.html",
-                                  {'result': result, 'user': user, 'log_info': log_info, 'successTag': True})
+                                  {'result': result, 'user': user, 'log_info': '成功', 'successTag': True})
     else:
         error_msg = '口令错！'
         return render_to_response('touch.html', {'error_msg': error_msg, 'permissions': allow_server})
